d8bf60fcaeb330220c743e368955f17d75dadcfb
programista10kpln <programista10kpln@proton.me>
2022-06-19 17:55:27 +0200

    groups

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 wzor = r'pilka'
 tekst = r'lodowka pilka kosz stol pilka pilka'
 
-print(re.match(wzor, tekst))  # szuka od początku
+print(re.match(wzor, tekst))  # szuka od początku (domyślnie)
 
 if re.match(r'.*' + wzor + r'.*', tekst):
     print('dopasowano')
@@ -32,5 +32,27 @@ print('\n')  # zaawansowane wzory
 
 if re.match('^[^A-Za-g]o.$', 'rot'):
     print('dopasowano')
+else:
+    print('nie dopasowano')
+
+print('\n')  # więcej operatorów
+
+if re.match('^[A-Z]$', 'H'):
+    print('dopasowano')
+else:
+    print('nie dopasowano')
+
+if re.match(r'^(.+)(@)(.+)(\.)(.+){2,}$', 'user@o2.pl'):
+    print('dobry e-mail')
+else:
+    print('to nie e-mail')
+
+print('\n')  # grupy - nawiasy
+
+wynik = re.match(r'^(?P<first>Hello) (World)$', 'Hello World')
+if wynik:
+    print(wynik.group(0))
+    print(wynik.group('first'))
+    print(wynik.groups())
 else:
     print('nie dopasowano')
