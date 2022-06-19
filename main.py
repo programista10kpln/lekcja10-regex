e4ac528e3363bc5e5ac144475bcd567217a0a0df
programista10kpln <programista10kpln@proton.me>
2022-06-19 16:28:25 +0200

    basic usage

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import re
 
-# match i search
+# match, search, findall i sub
 
 wzor = r'pilka'
-tekst = r'pilkakoszstol'
+tekst = r'lodowka pilka kosz stol pilka pilka'
 
 print(re.match(wzor, tekst))
+
+if re.match(r'.*' + wzor + r'.*', tekst):
+    print('dopasowano')
+else:
+    print('nie dopasowano')
+
+if re.search(wzor, tekst):
+    print('dopasowano')
+else:
+    print('nie dopasowano')
+
+print(re.findall(wzor, tekst))
+
+szukanie = re.search(wzor, tekst)
+if szukanie:
+    print(szukanie.group())
+    print(szukanie.start())
+    print(szukanie.end())
+    print(szukanie.span())
+tekst2 = re.sub(wzor, r'boisko', tekst)
+print(tekst2)
