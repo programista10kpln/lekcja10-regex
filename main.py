bd35c91c4ea3c982f7fbdf9bffdbdf8d7137c084
programista10kpln <programista10kpln@proton.me>
2022-06-19 16:57:41 +0200

    advanced match

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import re
 wzor = r'pilka'
 tekst = r'lodowka pilka kosz stol pilka pilka'
 
-print(re.match(wzor, tekst))
+print(re.match(wzor, tekst))  # szuka od początku
 
 if re.match(r'.*' + wzor + r'.*', tekst):
     print('dopasowano')
 else:
     print('nie dopasowano')
 
-if re.search(wzor, tekst):
+if re.search(wzor, tekst):  # szuka w całym tekście
     print('dopasowano')
 else:
     print('nie dopasowano')
 
-print(re.findall(wzor, tekst))
+print(re.findall(wzor, tekst))  # zwraca listę wszystkich wystąpień
 
 szukanie = re.search(wzor, tekst)
 if szukanie:
@@ -25,5 +25,12 @@ if szukanie:
     print(szukanie.start())
     print(szukanie.end())
     print(szukanie.span())
-tekst2 = re.sub(wzor, r'boisko', tekst)
+tekst2 = re.sub(wzor, r'boisko', tekst)  # podmienia
 print(tekst2)
+
+print('\n')  # zaawansowane wzory
+
+if re.match('^[^A-Za-g]o.$', 'rot'):
+    print('dopasowano')
+else:
+    print('nie dopasowano')
